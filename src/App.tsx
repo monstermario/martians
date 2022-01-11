370ded825fae406b29eb27ed101e76bc8559f4e2
trueflexible <dmilesandrew@gmail.com>
2022-01-11 00:57:05 -0700

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Land } from './pages/land';
 import { Migrate } from './pages/migrate';
-import { RiskyGame } from './pages/riskyGame';
+// import { RiskyGame } from './pages/riskyGame';
 import { Pouches } from './pages/pouches';
 
 import './App.css';
@@ -10,14 +10,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Switch>
-          <Route path="/risky-game" component={RiskyGame} />
+      <Router>
+        <Switch>
+          {/* <Route path="/risky-game" component={RiskyGame} /> */}
           <Route path="/pouches" component={Pouches} />
           <Route path="/land" component={Land} />
           <Route path="/" component={Migrate} />
-       </Switch>
-     </Router>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/pouches/index.tsx
+++ src/pages/pouches/index.tsx
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
 import { Header } from '../../components/header';
 import { WalletButton } from '../../components/ui/walletButton';
-import { WoodBox } from '../../components/ui/woodBox';
 import styles from './Pouches.module.scss';
 
 export const Pouches: React.FC = () => {
   const [hover, setHover] = useState<boolean>(false);
   return (
-      <div className={styles.back}>
-        <Header />
-        <div className={styles.pouches}>
-          <div className={styles.pouchesBody}>
-            <p className={styles.title}>WOOl Pouches</p>
-            <div className={styles.wallet}>
-              <div className={styles.walletBox}></div>
-              <WalletButton color='#b11d18'/>
-            </div>
+    <div className={styles.back}>
+      <Header />
+      <div className={styles.pouches}>
+        <div className={styles.pouchesBody}>
+          <p className={styles.title}>WOOl Pouches</p>
+          <div className={styles.wallet}>
+            <div className={styles.walletBox}></div>
+            <WalletButton color="#b11d18" />
           </div>
         </div>
       </div>
-     
+    </div>
   );
 };
